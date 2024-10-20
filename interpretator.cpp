1ce13a7094223d186818cf281abbeba08d4c562d
Елизавета Маслова <lizamushroom757@gmail.com>
2024-10-21 01:37:14 +0300

    execute in progress

--- interpretator.cpp
+++ interpretator.cpp
@@ -258,3 +258,45 @@ Instruction decode(Register bytes)
 	return instruction;
 }
 
+void execute(CpuState *cpu, Instruction *instruction) {
+	switch (instruction->opcode) 
+	{
+		case Opcode::kAdd: 
+			if (func7 == 0b0000000)
+			{
+				Register res = get_reg(cpu, instruction->rs1) + get_reg(cpu, instruction->rs2);
+			}
+			else if (func7 == 0b0100000)
+			{
+				Register res = get_reg(cpu, instruction->rs1) - get_reg(cpu, instruction->rs2);
+			}
+			set_reg(cpu, instruction->rd, res);
+			cpu->pc += 4;
+			break;
+		case Opcode::kAddi:
+			Register res = get_reg(cpu, instruction->rs1) + instuction->imm;
+			set_reg(cpu, instruction->rd, res);
+			cpu->pc += 4;
+			break;
+		case Opcode::kEBreak: 
+			cpu->is_finished = true;
+			break;
+		case Opcode::kJal: 
+			set_reg(cpu, instruction->rd, cpu->pc + 4);
+			Register jump_target_address = cpu->pc + instruction->imm;
+        		cpu->pc = jump_target_address;
+			break;
+		case Opcode::kJalr:
+			set_reg(cpu, instruction->rd, cpu->pc + 4);
+                        Register jump_target_address = (cpu->pc + instruction->imm) & 0xFFFFFFFE;
+                        cpu->pc = jump_target_address; 
+        		break;
+		case Opcode::kStore: 
+        		cpu->pc += 4;
+			break;
+		case Opcode::kBeq: 
+        		break;
+		default:
+			printf("Unknown instruction");
+    }
+}
